1054c6aa292c21d36398b4a32cc1c8057888b878
ItsKek <75073133+ItsKek@users.noreply.github.com>
2022-01-04 23:27:55 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MCHostResolver',
+    version='1.2',
+    packages=['MCHostResolver'],
+    url='github.com/itskek/MCHostResolver',
+    license='MIT',
+    author='itskekoff',
+    author_email='itskekoff@gmail.com',
+    description='A module for resolving MC hostnames'
+)
