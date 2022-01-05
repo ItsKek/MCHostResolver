3313ceca0c45d20c957904d179426fd18c132ee2
ItsKek <75073133+ItsKek@users.noreply.github.com>
2022-01-05 13:50:06 +1000

    v1.5
    v1.5

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='MCHostResolver',
-    version='1.2',
+    version='1.5',
     packages=['MCHostResolver'],
-    url='github.com/itskek/MCHostResolver',
+    url='https://github.com/itskek/MCHostResolver',
     license='MIT',
     author='itskekoff',
     author_email='itskekoff@gmail.com',
-    description='A module for resolving MC hostnames'
+    description='A module for resolving MC hostnames',
+    install_requires=['colorama', 'dnspython'],
+    long_description = long_description,
+    long_description_content_type = "text/markdown"
 )
